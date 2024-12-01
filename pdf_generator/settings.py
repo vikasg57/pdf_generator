afe9fcfaad2d4e6bf47264d079f88bd433f5e012
Vikas Gaikwad <vgaikwad299@gmail.com>
2024-12-01 17:13:58 +0530

    empty

--- pdf_generator/settings.py
+++ pdf_generator/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
