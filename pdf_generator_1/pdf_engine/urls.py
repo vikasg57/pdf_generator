8516cca7d13d016882f7eca247ca78404e2d5e2f
Vikas Gaikwad <vgaikwad299@gmail.com>
2024-12-01 17:04:22 +0530

    strucutr fix

--- pdf_generator_1/pdf_engine/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-from .views import PDFGeneratorView
-
-urlpatterns = [
-    path("<uuid:template_id>/generate/", PDFGeneratorView.as_view(), name="get_view"),
-]
